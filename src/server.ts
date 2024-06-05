f33e30acce35410944bcf7e14a0dd3865bac0f10
renansilva15 <renangabrielsilva150@gmail.com>
2024-06-05 16:17:57 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get("/", (req, res) => {
+  return {status: "Ok"}
+})
+
+app.listen({port: 3333}).then((url) => {console.log(`Server listen ${url}`)});
